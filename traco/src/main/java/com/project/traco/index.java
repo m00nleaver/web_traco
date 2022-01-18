9d5080de9b07ba727ce3a33915262841d17752c7
jaehyung0 <94wogud@daum.net>
2022-01-18 15:41:09 +0900

    package명 변경

--- traco/src/main/java/com/project/traco/index.java
+++ traco/src/main/java/com/project/traco/index.java
@@ -1,1 +1,21 @@
 package com.project.traco;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/name.do")
+public class index extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/name.jsp");
+
+		dispatcher.forward(req, resp);
+	}
+}
